7a5dde1964034cff2b979ffc52247945051aa21e
softwareinsights <cesar@softwareinsights.com.mx>
2018-03-12 13:33:25 -0600

    conf amazon

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Configuration {
 
-  Server: string = 'http://localhost:3000/';
+  Server: string = 'http://ec2-52-34-75-49.us-west-2.compute.amazonaws.com:3000/';
   ApiUrl: string = '';
   // imageServerWithApiUrl: string = 'http://aidihosting.com/proyectos/proyectura_api/v1/';
   imageServerWithApiUrl: string = 'http://localhost/proyectura_api/v1/';
